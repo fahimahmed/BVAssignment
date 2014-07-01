0f626c8716dddaf99a937230f655a53674131d56
fahimahmed <fahim.ahmed1988@gmail.com>
2014-07-01 20:52:01 +0600

    Update ProductSyncService.java

--- src/com/fahimahmed/bv/syncadapter/ProductSyncService.java
+++ src/com/fahimahmed/bv/syncadapter/ProductSyncService.java
@@ -5,11 +5,6 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
-/**
- * Service to handle Account sync. This is invoked with an intent with action
- * ACTION_AUTHENTICATOR_INTENT. It instantiates the com.udinic.sync_adapter_example_app.syncadapter and returns its
- * IBinder.
- */
 public class ProductSyncService extends Service {
 
     private static final Object sSyncAdapterLock = new Object();
