a13d0d28fdffacd6e92135da73c162bf6e1735a3
fahimahmed <fahim.ahmed1988@gmail.com>
2014-07-01 20:54:38 +0600

    Update ProductsContract.java

--- src/com/fahimahmed/bv/contentprovider/ProductsContract.java
+++ src/com/fahimahmed/bv/contentprovider/ProductsContract.java
@@ -2,11 +2,6 @@ package com.fahimahmed.bv.contentprovider;
 
 import android.net.Uri;
 
-/**
- * Holds the API to the content provider
- *
- * Created by Udini on 7/2/13.
- */
 public class ProductsContract {
 
     public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.bv.products";
